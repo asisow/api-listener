637d15ef3a79c47db7d0064bba2b1ce31acb07d4
asisow <38430905+asisow@users.noreply.github.com>
2019-02-12 15:29:11 +0300

    Gifts.ru inspector

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+const login = 'mail@rico-design.info';
+const password = '123';
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+    const api = 'http://' + login + ':' + password + '@api2.gifts.ru/export/v2/catalogue/product.xml';
+    console.log(api);
+    request(api, (err, response, body) => {
+        res.send(response.body);
+    });
+});
+app.listen(8080, () => console.log('Goliath online'));
