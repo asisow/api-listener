1381dbecf7ac83228e7a39692e22c29c0cb1cf40
Andrey <asisow@gmail.com>
2019-02-13 00:59:42 +0300

    Get parameters for POST request

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const login = 'mail@rico-design.info';
-const password = '123';
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -13,10 +14,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+    const login = req.body.login;
+    const password = req.body.password;
+    console.log(login, password);
     const api = 'http://' + login + ':' + password + '@api2.gifts.ru/export/v2/access'//'@api2.gifts.ru/export/v2/catalogue/product.xml';
     console.log(api);
     request(api, (err, response, body) => {
-      console.log(body);
         res.send(response.body);
     });
 });
